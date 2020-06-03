58e9b07e314a5819e970a1be3f0d987e20d3525b
Alexander <Alexandr_kitchenko@mail.ru>
2020-06-03 15:38:53 +0600

    commits

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,7 +115,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'shit-exchange.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV['DEFAULT_HOST'] }
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
