52ab8f1a7cb1a44ff9a938baf1ce1ed4cf03b352
Alexander <Alexandr_kitchenko@mail.ru>
2020-06-04 13:01:19 +0600

    refact

--- app/javascript/custom/modal.js
+++ app/javascript/custom/modal.js
@@ -2,17 +2,17 @@ $(document).ready(() => {
 
   // fills .modal-errors div in sign-in-user modal
   $("form#sign-in-user")
-  .on("ajax:success", function(event) {
-    $(this).parents('.modal').modal('hide');
-    location.reload();
-  })
-  .on("ajax:error", function(event) {
-    const [errors, status, xhr] = event.detail
-    const error_messages =  $.map(errors, function(value, key) {
-      return `<p class="text-danger mb-1">${value}</p>`;
-    }).join("");
-    return $('#sign-in-modal').find('.modal-errors').html(error_messages);
-  });
+    .on("ajax:success", function(event) {
+      $(this).parents('.modal').modal('hide');
+      location.reload();
+    })
+    .on("ajax:error", function(event) {
+      const [errors, status, xhr] = event.detail
+      const error_messages =  $.map(errors, function(value, key) {
+        return `<p class="text-danger mb-1">${value}</p>`;
+      }).join("");
+      return $('#sign-in-modal').find('.modal-errors').html(error_messages);
+    });
 
   // fills .modal-errors divs in sign-up-user modal
   $("form#sign-up-user")
@@ -49,23 +49,23 @@ $(document).ready(() => {
 
   // fills .modal-errors divs in other modal
   $("form#new-password-user, form#edit-password-user, form#new-conformation-user")
-  .on("ajax:success", function(event) {
-    $(this).parents('.modal').modal('hide');
-  })
-  .on("ajax:error", function(event) {
-    const errors = event.detail[0]["errors"]
-    const error_messages =  $.map(errors, function(value, key) {
-      return `<p class="text-danger mb-1">${key} ${value}</p>`;
-    }).join("");
-    return $('.modal').find('.modal-errors').html(error_messages);
-  });
+    .on("ajax:success", function(event) {
+      $(this).parents('.modal').modal('hide');
+    })
+    .on("ajax:error", function(event) {
+      const errors = event.detail[0]["errors"]
+      const error_messages =  $.map(errors, function(value, key) {
+        return `<p class="text-danger mb-1">${key} ${value}</p>`;
+      }).join("");
+      return $('.modal').find('.modal-errors').html(error_messages);
+    });
 
   //opens modal after ajax:succes
   $("form#new-password-user")
-  .on("ajax:success", function(event) {
-    $(this).parents('.modal').modal('hide');
-    $("#password-instructions-modal").modal("show");
-  })
+    .on("ajax:success", function(event) {
+      $(this).parents('.modal').modal('hide');
+      $("#password-instructions-modal").modal("show");
+    })
 
   // clears error and input fields data after closing the modal
   $('.modal').on('hide.bs.modal', function () {
